277c9d545c452365bf5b6d9efc12d98a145a4c27
rafinhaa10 <00001088423401sp@al.educacao.sp.gov.br>
2024-11-08 13:17:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futebol',
+    'Em que ano o Neymar foi campeão da Champion League?',
+    '2015'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem foi o ultimo time campeão Mundial do Brasil?',
+    'Corinthians'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual foi o jodador que fez mais gols do Corinthians?',
+    'Claudio(305 gols)'
+)
+
+criaCartao(
+    'Futebol',
+    'Em que ano o Pelé nasceu?',
+    '1940'
+)
